3135c2bfe73849196c86ec04adcdd5416491268f
Mostafa Rasouli <Kasraco@gmail.com>
2024-08-06 02:24:52 +0330

    change and setting class for layout pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,20 @@
-import { JetBrains_Mono } from "next/font/google";
+import { Oswald, Open_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "./StairTransition";
+import MyMenu from "@/components/MyMenu";
 
-const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800"], variable: '--font-jetbrainsMono' });
+const oswald = Oswald({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+  variable: "--font-oswald",
+});
+const opensans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-opensans",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -14,10 +24,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>
-        <Header />
-        <StairTransition />
-        <PageTransition>{children}</PageTransition>
+      <body
+        className={` ${oswald.variable} container w-full h-full min-h-screen pt-[4em] px-[2em]`}
+      >
+        <MyMenu />
+        {children}
       </body>
     </html>
   );
